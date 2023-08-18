e3045eaf36fc657689dd7b69012e96ef082e6b71
Denis Kenzior <denkenz@gmail.com>
2023-08-18 09:17:18 -0500

    radio-settings: Port drivers to use OFONO_ATOM_DRIVER_BUILTIN

--- ofono/drivers/qmimodem/radio-settings.c
+++ ofono/drivers/qmimodem/radio-settings.c
@@ -30,8 +30,7 @@
 #include "qmi.h"
 #include "nas.h"
 #include "dms.h"
-
-#include "qmimodem.h"
+#include "util.h"
 
 struct settings_data {
 	struct qmi_service *nas;
@@ -286,7 +285,6 @@ static void qmi_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name		= "qmimodem",
 	.probe		= qmi_radio_settings_probe,
 	.remove		= qmi_radio_settings_remove,
 	.set_rat_mode	= qmi_set_rat_mode,
@@ -294,12 +292,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.query_available_rats = qmi_query_available_rats,
 };
 
-void qmi_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void qmi_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, qmimodem, &driver)
--- ofono/drivers/rilmodem/radio-settings.c
+++ ofono/drivers/rilmodem/radio-settings.c
@@ -37,8 +37,7 @@
 #include <ofono/radio-settings.h>
 
 #include "gril.h"
-
-#include "rilmodem.h"
+#include "rilutil.h"
 
 /* Preferred network types */
 #define PREF_NET_TYPE_GSM_WCDMA 0
@@ -453,7 +452,6 @@ static void ril_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= RILMODEM,
 	.probe			= ril_radio_settings_probe,
 	.remove			= ril_radio_settings_remove,
 	.query_rat_mode		= ril_query_rat_mode,
@@ -464,12 +462,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.query_available_rats	= ril_query_available_rats
 };
 
-void ril_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void ril_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, rilmodem, &driver)
