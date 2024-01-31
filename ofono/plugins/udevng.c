a9e74bc03af6f1cf8e7830ab00400e58195f0e93
Sean Nyekjaer <sean@geanix.com>
2024-01-31 14:57:28 +0100

    udevng: Add support for Quectel EG96 modem

--- ofono/plugins/udevng.c
+++ ofono/plugins/udevng.c
@@ -1891,6 +1891,8 @@ static struct {
 	{ "quectelqmi",	"qcserial",	"2c7c", "0121"	},
 	{ "quectelqmi",	"qmi_wwan",	"2c7c", "0125"	},
 	{ "quectelqmi",	"qcserial",	"2c7c", "0125"	},
+	{ "quectelqmi",	"qmi_wwan",	"2c7c", "0195"	},
+	{ "quectelqmi",	"qcserial",	"2c7c", "0195"	},
 	{ "quectelqmi",	"qmi_wwan",	"2c7c", "0296"	},
 	{ "quectelqmi",	"qcserial",	"2c7c", "0296"	},
 	{ "ublox",	"cdc_acm",	"1546", "1010"	},
